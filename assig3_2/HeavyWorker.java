47d82489e8d85ffed66678fe4822fc1c07399d2a
Vaturi <omri1991@gmail.com>
2022-12-27 20:19:20 +0200

    Finishing 2Q

--- assig3_2/HeavyWorker.java
+++ assig3_2/HeavyWorker.java
@@ -2,38 +2,56 @@ package assig3_2;
 
 public class HeavyWorker {
 
-	public void section1() {
-		System.out.println(Thread.currentThread().getName() + " is in section1");
-		try {
-			/* sleep to simulate some work... */
-			Thread.sleep(500);
-		} catch (InterruptedException e) {}
-		System.out.println(Thread.currentThread().getName() + " leaving section1");
-	}
-	
-	public void section2() {
-		System.out.println(Thread.currentThread().getName() + " is in section2");
-		try {
-			/* sleep to simulate some work... */
-			Thread.sleep(500);
-		} catch (InterruptedException e) {}
-		System.out.println(Thread.currentThread().getName() + " leaving section2");
-	}
-	
-	public void workA() {
-		System.out.println(Thread.currentThread().getName() + " doing workA");
-		
-		/* section1() can be called from up to 3 threads at the same time */
-		section1();
-		/* section2() can be called from one thread out of the 3 above    */		
-		section2();
-		
-	}
-	
-	public void workB() {
+    private MySemaphore s1 = new MySemaphore(0);
+    private MySemaphore s2 = new MySemaphore(0);
+    private MySemaphore s3 = new MySemaphore(0);
 
-		/* this will be printed only after workA() done at least once */
-		System.out.println(Thread.currentThread().getName() + " doing workB");
-	}
-	
+
+    public void section1() {
+        System.out.println(Thread.currentThread().getName() + " is in section1");
+        try {
+            /* sleep to simulate some work... */
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+        }
+        System.out.println(Thread.currentThread().getName() + " leaving section1");
+    }
+
+    public void section2() {
+        System.out.println(Thread.currentThread().getName() + " is in section2");
+        try {
+            /* sleep to simulate some work... */
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+        }
+        System.out.println(Thread.currentThread().getName() + " leaving section2");
+    }
+
+    public void workA() {
+        System.out.println(Thread.currentThread().getName() + " doing workA");
+
+        s1.up();
+        section1();
+        s1.down();
+        s2.up();
+        section1();
+        section2();
+        s2.down();
+        s3.up();
+        section1();
+        s3.up();
+        s3.up();
+
+    }
+
+    public void workB() {
+        s3.down();
+        s3.down();
+        s3.up();
+            System.out.println(Thread.currentThread().getName() + " doing workB");
+
+
+    }
 }
+
+
